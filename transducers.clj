2a14f124221b61764d78d5ceb2229bc89b7156f4
chopraapooja <all.is.well.chopra@gmail.com>
2016-09-04 11:16:28 +0530

    Adds map with prepend recursion

--- transducers.clj
+++ transducers.clj
@@ -2,6 +2,10 @@
 
 (defn look [x] (prn x) x)
 
+;---------------------------------------------
+;BASIC VERSION
+;Map with append recursion
+
 (defn my-map
   ([f coll]
    (my-map [] f coll))
@@ -15,3 +19,20 @@
 (my-map inc [1 2 3])
 
 (my-map inc [])
+
+(my-map inc nil)
+
+
+;---------------------------------------------
+;Map with prepend recursion
+
+(defn my-map [f coll]
+  (if (seq coll)
+    (cons (-> coll first f) (my-map f (rest coll)))
+    []))
+
+(my-map inc [1 2 3])
+
+(my-map inc [])
+
+(my-map inc nil)
