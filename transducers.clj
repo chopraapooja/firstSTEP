c2d8e2de9120d4da17b6acffec3aa8b6632c796e
chopraapooja <all.is.well.chopra@gmail.com>
2016-09-04 10:49:59 +0530

    Refactores a bit

--- transducers.clj
+++ transducers.clj
@@ -2,16 +2,16 @@
 
 (defn look [x] (prn x) x)
 
-(defn- apply-map [result f coll]
-  (if (seq coll)
+(defn my-map
+  ([f coll]
+   (my-map [] f coll))
+  ([result f coll]
+   (if (seq coll)
     (apply-map (conj result (-> coll first f))
                f
                (rest coll))
-    result))
-
-(defn my-map [f coll]
-  (apply-map [] f coll))
+    result)))
 
 (my-map inc [1 2 3])
 
-(look 3)
+(my-map inc [])
